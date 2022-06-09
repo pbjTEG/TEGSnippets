45fc5280a100068a5f364de9a1af250b0347dec6
Paul B. Joiner <paulj@engageyourcause.com>
2022-06-09 15:08:20 -0500

    remove obsolete browser-specific code

--- JS/Array.includes_polyfill.js
+++
@@ -1,9 +0,0 @@
-// Array.includes() polyfill for IE10
-if (!Array.prototype.includes) {
-	Object.defineProperty(Array.prototype, "includes", {
-		enumerable: false, value: function(obj) {
-			var newArr = this.filter(function(el) { return el == obj; });
-			return newArr.length > 0;
-		}
-	});
-}
