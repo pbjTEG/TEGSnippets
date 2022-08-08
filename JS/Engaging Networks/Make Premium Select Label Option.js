e184d02681359d0c4838e3a91394ce336f8362cf
Paul B. Joiner <paulj@engageyourcause.com>
2022-08-08 10:18:30 -0500

    make it more generic

--- JS/Engaging Networks/Make Premium Select Label Option.js
+++ JS/Engaging Networks/Make Premium Select Label Option.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function() {
 	window.TEGPObserver = new MutationObserver(function(mutationsList, observer) {
 		if (jQuery('.en__pg select option[value=""]').length === 0) {
 			// noinspection StringLiteralBreaksHTMLJS
-			jQuery('.en__pg select').prepend('<option value="" selected="selected">select your bottle design</option>');
+			jQuery('.en__pg select').prepend('<option value="" selected="selected">make a selection</option>');
 		}
 	});
 	TEGPObserver.observe(jQuery('.en__pgList')[0],
